0bd4ed50185268ccb85572419a65460e061be62f
SherbakovSAF <sherbakovsaf@gmail.com>
2023-10-07 17:02:27 +0300

    Сборка компонентов в App.vue

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,47 +1,30 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import DropDown from './components/DropDown.vue';
+import SubmitBtn from './components/SubmitBtn.vue';
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div id="app">
+    <div id="input-panel">
+    <DropDown />
+    <SubmitBtn />
+  </div>
+  </div>
+  
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
+#app {
+  max-width: 1000px;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+#input-panel{
+  display: flex;
+  grid-column-gap: 2em;
 }
 </style>
